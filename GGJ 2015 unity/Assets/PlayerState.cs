4229edcec2a084ca811322c743e7fd1dddee99f1
Louis Guillemin <louisguillemin@msn.com>
2015-01-24 00:43:27 +0100

    no message

---
+++ GGJ 2015 unity/Assets/PlayerState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour {
+	
+	[HideInInspector]
+	public bool _canMove;
+	
+	[HideInInspector]
+	public int _Score;
+	
+	// Use this for initialization
+	void Start () {
+		_canMove=false;
+		_Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
