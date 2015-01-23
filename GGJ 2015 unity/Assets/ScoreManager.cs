26250c774fad5c5971904416347a464c142a4b77
Virgule222 <virgilelions@hotmail.fr>
2015-01-24 00:29:06 +0100

    Prefabs+material+zones

---
+++ GGJ 2015 unity/Assets/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	private GameObject _LastGoalHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider _collider){
+		if(_collider.tag == "Goal" && _LastGoalHit != _collider.gameObject){
+			_LastGoalHit = _collider.gameObject;
+			GetComponent<PlayerState>()._Score += 1;
+		}
+	}
+}
