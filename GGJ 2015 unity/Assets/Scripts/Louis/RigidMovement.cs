ac9db6110f3470db7204d0b537531366e443012c
Louis Guillemin <louisguillemin@msn.com>
2015-01-24 03:18:56 +0100

    Commit Louis: movement et rotation dans les triggers

--- GGJ 2015 unity/Assets/Scripts/Louis/RigidMovement.cs
+++ GGJ 2015 unity/Assets/Scripts/Louis/RigidMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using XInputDotNetPure;
 
-public class BasicMovement : MonoBehaviour {
+public class RigidMovement : MonoBehaviour {
 
 	bool playerIndexSet = false;
 	public PlayerIndex playerIndex;
@@ -19,10 +19,11 @@ public class BasicMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		gameObject.transform.Find("Pointe").renderer.enabled=false;
+
 		v_initSpeed=v_speed;
 		reoriented=false;
-		isInTrigger=false;
-//		gameObject.rigidbody.AddForce(gameObject.transform.forward*v_speed);
+		gameObject.rigidbody.AddForce(gameObject.transform.forward*v_speed);
 	}
 	
 	// Update is called once per frame
@@ -39,61 +40,57 @@ public class BasicMovement : MonoBehaviour {
 		if(isInTrigger==false){
 			gameObject.transform.forward = gameObject.rigidbody.velocity;
 		}
+
 	}
 
 	void FixedUpdate(){
-//		if(isInTrigger==false){
 			MovementEveryFrame();
-//		}else{
-//			if(prevState.Buttons.RightShoulder==ButtonState.Released && state.Buttons.RightShoulder == ButtonState.Pressed){
-//				MovementEveryFrame();
-//			}
-//		}
+
 	}
 	
 
 	void MovementEveryFrame(){
-		//ca mais fait pour du frame par frame
-		Vector3 direction = new Vector3(0,0,0);
-		direction=gameObject.transform.forward;
-		direction.Normalize();
-		
-		_movement = direction*v_speed;
-		rigidbody.AddForce(_movement);
-//		gameObject.rigidbody.AddForce(gameObject.transform.forward*v_speed, ForceMode.Force);
-
 	}
 
 	void Movement(){
-//		gameObject.rigidbody.velocity=Vector3.zero;
-////		gameObject.rigidbody.AddForce(gameObject.transform.forward*v_speed);
-//
-//
+		gameObject.rigidbody.velocity=Vector3.zero;
+		gameObject.rigidbody.AddForce(gameObject.transform.forward*v_speed);
+
+
 	}
 
 	void OnTriggerStay(Collider collision){
 		isInTrigger=true;
-		v_speed-=v_speedLoss*Time.deltaTime;
+//		v_speed-=v_speedLoss*Time.deltaTime;
+		gameObject.transform.Find("Pointe").renderer.enabled=true;
+
+		gameObject.rigidbody.velocity *= 0.995f;
+
 		if(collision.gameObject.tag.Equals("TriggerZone")){
-			if(state.ThumbSticks.Right.X!=0 || state.ThumbSticks.Right.Y!=0){
-				float angle = Mathf.Atan2 (state.ThumbSticks.Right.Y, state.ThumbSticks.Right.X) * Mathf.Rad2Deg;
+			if(state.ThumbSticks.Left.X!=0 || state.ThumbSticks.Left.Y!=0){
+				float angle = Mathf.Atan2 (state.ThumbSticks.Left.Y, state.ThumbSticks.Left.X) * Mathf.Rad2Deg;
 				this.transform.rotation = Quaternion.Euler (new Vector3(0, -angle+90, 0));
-				if(prevState.Buttons.RightShoulder==ButtonState.Released && state.Buttons.RightShoulder == ButtonState.Pressed){
-//					isInTrigger=false;
+
+				//input for reorientation
+				if(state.Buttons.RightShoulder == ButtonState.Pressed){
+					if(reoriented==false){
+						reoriented=true;
+						Movement();
+					}
 				}
-//					if(reoriented==false){
-//						reoriented=true;
-//						Movement();
-//					}
-//				}
 			}
 		}
 	}
 
 	void OnTriggerExit(Collider collision){
-		isInTrigger=false;
+		gameObject.transform.Find("Pointe").renderer.enabled=false;
+
 		v_speed=v_initSpeed;
+		isInTrigger=false;
 		if(collision.gameObject.tag.Equals("TriggerZone")){
+			if(reoriented==false){
+				Movement();
+			}
 			reoriented=false;
 		}
 	}
